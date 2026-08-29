9a13afe01304868a844309e27c8317164f1e28f1
RomanMdesign <piol.aljay.arroyo1@gmail.com>
2026-08-29 19:15:39 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import "leaflet/dist/leaflet.css";
-import "./styles.css";
-
 import App from "./App";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
